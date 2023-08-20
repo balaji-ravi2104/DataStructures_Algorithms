f7ed91d7cd4d2b02696469dcb265f565f64d6675
balaji <srbalaji435@gmail.com>
2023-08-20 11:52:26 +0530

    New Commit

--- Binary Tree/src/BinaryTree.java
+++ Binary Tree/src/BinaryTree.java
@@ -1,21 +1,5 @@
 import java.util.*;
 
-class TreeNode {
-     int val;
-      TreeNode left;
-      TreeNode right;
-      public TreeNode() {}
-      public TreeNode(int val) {
-          this.val = val;
-          this.left = null;
-          this.right = null;
-      }
-      public TreeNode(int val, TreeNode left, TreeNode right) {
-          this.val = val;
-          this.left = left;
-          this.right = right;
-      }
-}
 public class BinaryTree {
     public static TreeNode buildTree(){
         Scanner sc = new Scanner(System.in);
